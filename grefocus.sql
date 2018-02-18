6b64529887f556023d766dbd0a8b49759bf5af90
Yash Mahajan <ymahajan67@gmail.com>
2018-02-18 18:02:10 +0530

    Added Sql file

---
+++ grefocus.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 18, 2018 at 01:31 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `grefocus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vocab`
+--
+
+CREATE TABLE `vocab` (
+  `id` int(11) NOT NULL,
+  `word` varchar(255) NOT NULL,
+  `synonym` varchar(255) NOT NULL,
+  `antonym` varchar(255) NOT NULL,
+  `sentence` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vocab`
+--
+
+INSERT INTO `vocab` (`id`, `word`, `synonym`, `antonym`, `sentence`) VALUES
+(1, 'Abate', 'Subside', 'Alienate', 'Rather than leave immediately, they waited for the storm to abate.'),
+(2, 'Aberrant', 'Abnormal', '--', 'Given the aberrant nature of the data, we came to doubt the validity of the entire experiment.'),
+(3, 'Abeyance', 'Suspended Action', 'Continuance', 'The deal was held in abeyance untill her arrival.'),
+(4, 'Abscond', 'Depart Secretly', 'Appear', 'The teller absconded with the bonds and was not found.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `vocab`
+--
+ALTER TABLE `vocab`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `vocab`
+--
+ALTER TABLE `vocab`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
